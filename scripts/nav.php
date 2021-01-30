db5889908250a00be468f8c64addca5c25cfaafa
James <4dunsj71@solent.ac.uk>
2021-01-30 12:51:18 +0000

    added bootstrap, added nav/head

---
+++ scripts/nav.php
@@ -0,0 +1,35 @@
+<?php
+?>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+  <a class="navbar-brand" href="#">company .ltd</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">about</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pricing.php">pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="booking.php">booking</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="gallery.php">gallery</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="staff.php">staff</a>
+      </li>
+		</ul>
+  </div>
+</nav>
