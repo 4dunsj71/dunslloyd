572024318b16861066407345dd6db18c787d70ba
James <4dunsj71@solent.ac.uk>
2021-01-30 13:43:25 +0000

    added more to index

--- index.php
+++ index.php
@@ -12,34 +12,40 @@
     <title>Document</title>
 </head>
 <body>
+
+    <br><br><br><br>
+    <div class ="container-fluid text-center">
+        <h1 class="display-1">Lorem ipsum</h1>
+    </div>
     <br><br><br><br>
     <div class="container">
-        <div class="container">
-            <div class ="jumbotron">
-                <h1 class="display-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-                <p class ="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis officia vitae maxime nobis similique! Neque quibusdam dolores voluptatibus voluptas accusantium cupiditate tenetur amet! Sapiente tempore officiis, illum animi adipisci necessitatibus!</p>
-                <a class="btn btn-primary btn-lg" href="#" role ="button"> Learn More</a>
+        <div class="row">
+            <div class ="col">
+                <div class="container">
+                    <div class ="jumbotron">
+                        <h1 class="display-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                        <p class ="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis officia vitae maxime nobis similique! Neque quibusdam dolores voluptatibus voluptas accusantium cupiditate tenetur amet! Sapiente tempore officiis, illum animi adipisci necessitatibus!</p>
+                        <a class="btn btn-primary btn-lg" href="#" role ="button"> Learn More</a>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="container">
-            <div class ="jumbotron">
-                <h1 class="display-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-                <p class ="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis officia vitae maxime nobis similique! Neque quibusdam dolores voluptatibus voluptas accusantium cupiditate tenetur amet! Sapiente tempore officiis, illum animi adipisci necessitatibus!</p>
-                <a class="btn btn-primary btn-lg" href="#" role ="button"> Learn More</a>
+            <div class ="col">
+                <div class="container">
+                    <div class ="jumbotron">
+                        <h1 class="display-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                        <p class ="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis officia vitae maxime nobis similique! Neque quibusdam dolores voluptatibus voluptas accusantium cupiditate tenetur amet! Sapiente tempore officiis, illum animi adipisci necessitatibus!</p>
+                        <a class="btn btn-primary btn-lg" href="#" role ="button"> Learn More</a>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="container">
-            <div class ="jumbotron">
-                <h1 class="display-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-                <p class ="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis officia vitae maxime nobis similique! Neque quibusdam dolores voluptatibus voluptas accusantium cupiditate tenetur amet! Sapiente tempore officiis, illum animi adipisci necessitatibus!</p>
-                <a class="btn btn-primary btn-lg" href="#" role ="button"> Learn More</a>
-            </div>
-        </div>
-        <div class="container">
-            <div class ="jumbotron">
-                <h1 class="display-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-                <p class ="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis officia vitae maxime nobis similique! Neque quibusdam dolores voluptatibus voluptas accusantium cupiditate tenetur amet! Sapiente tempore officiis, illum animi adipisci necessitatibus!</p>
-                <a class="btn btn-primary btn-lg" href="#" role ="button"> Learn More</a>
+            <div class ="col">
+                <div class="container">
+                    <div class ="jumbotron">
+                        <h1 class="display-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                        <p class ="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis officia vitae maxime nobis similique! Neque quibusdam dolores voluptatibus voluptas accusantium cupiditate tenetur amet! Sapiente tempore officiis, illum animi adipisci necessitatibus!</p>
+                        <a class="btn btn-primary btn-lg" href="#" role ="button"> Learn More</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
