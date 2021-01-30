7cabcdcce99add2e6d2a307a214501cccf820926
James <4dunsj71@solent.ac.uk>
2021-01-30 13:30:02 +0000

    index modified

--- index.php
+++ index.php
@@ -12,6 +12,36 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <br><br><br><br>
+    <div class="container">
+        <div class="container">
+            <div class ="jumbotron">
+                <h1 class="display-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                <p class ="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis officia vitae maxime nobis similique! Neque quibusdam dolores voluptatibus voluptas accusantium cupiditate tenetur amet! Sapiente tempore officiis, illum animi adipisci necessitatibus!</p>
+                <a class="btn btn-primary btn-lg" href="#" role ="button"> Learn More</a>
+            </div>
+        </div>
+        <div class="container">
+            <div class ="jumbotron">
+                <h1 class="display-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                <p class ="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis officia vitae maxime nobis similique! Neque quibusdam dolores voluptatibus voluptas accusantium cupiditate tenetur amet! Sapiente tempore officiis, illum animi adipisci necessitatibus!</p>
+                <a class="btn btn-primary btn-lg" href="#" role ="button"> Learn More</a>
+            </div>
+        </div>
+        <div class="container">
+            <div class ="jumbotron">
+                <h1 class="display-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                <p class ="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis officia vitae maxime nobis similique! Neque quibusdam dolores voluptatibus voluptas accusantium cupiditate tenetur amet! Sapiente tempore officiis, illum animi adipisci necessitatibus!</p>
+                <a class="btn btn-primary btn-lg" href="#" role ="button"> Learn More</a>
+            </div>
+        </div>
+        <div class="container">
+            <div class ="jumbotron">
+                <h1 class="display-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                <p class ="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis officia vitae maxime nobis similique! Neque quibusdam dolores voluptatibus voluptas accusantium cupiditate tenetur amet! Sapiente tempore officiis, illum animi adipisci necessitatibus!</p>
+                <a class="btn btn-primary btn-lg" href="#" role ="button"> Learn More</a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
